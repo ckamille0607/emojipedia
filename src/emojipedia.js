d1075ec5d9a1f5f5cf76cf09a6160e2e77949518
chelseakamille <ckamille0607@gmail.com>
2022-04-03 00:10:34 -0700

    first commit

---
+++ src/emojipedia.js
@@ -0,0 +1,67 @@
+const emojipedia = [
+    {
+        id: 1,
+        emoji: "🫠",
+        name: "Melting Face",
+        meaning:
+            "A yellow smiley face melting into a puddle. The eyes and mouth slip down the face, yet still maintain a distorted smile. This quality lends this emoji to sarcasm."
+    },
+    {
+        id: 2,
+        emoji: "🫢",
+        name: "Face with Open Eyes and Hand Over Mouth",
+        meaning:
+            "A yellow face with open (non-smiling) eyes and a hand over the mouth, used to convey shock or surprise."
+    },
+    {
+        id: 3,
+        emoji: "🫣",
+        name: "Face with Peeking Eye",
+        meaning:
+            "A yellow face with its hands over its eyes and raised eyebrows. One wide eye is peeking through the fingers."
+    },
+    {
+        id: 4,
+        emoji: "🫤",
+        name: "Face with Diagonal Mouth",
+        meaning:
+            "A yellow face with a closed slanted mouth. Can be used to express skepticism, confusion, or frustration."
+    },
+    {
+        id: 5,
+        emoji: "🫥",
+        name: "Dotted Line Face",
+        meaning:
+            "A muted yellow neutral face with a dotted or dashed outline. An established comic book trope, dotted lines around a character can represent someone that is invisible or hidden."
+    },
+    {
+        id: 6,
+        emoji: "🥹",
+        name: "Face Holding Back Tears",
+        meaning:
+            "A yellow face with tears welling up at the bottom of its two large eyes. May be used to express a range of emotions including sadness, anger, embarrassment, admiration, and gratitude."
+    },
+    {
+        id: 7,
+        emoji: "🫦",
+        name: "Biting Lip",
+        meaning:
+            "A mouth with the top teeth biting down on one side of the lower lip. Used in romantic contexts to express flirtation or arousal. Can be used to express anticipation or excitement in general."
+    },
+    {
+        id: 8,
+        emoji: "🫶",
+        name: "Heart Hands",
+        meaning:
+            "Two hands forming a heart shape. Used to express love and support."
+    },
+    {
+        id: 9,
+        emoji: "🫰",
+        name: "Hand with Index Finger and Thumb Crossed",
+        meaning:
+            "A hand with the index finger and thumb crossed and the rest of the fingers making a fist. Can be used in various contexts to represent the fingers snapping, love, or money."
+    }
+];
+
+export default emojipedia;
